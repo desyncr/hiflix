9ae74c13cdcdd6ead0c7c21d91ef124811958799
DC* <dc@syncr.com.ar>
2014-12-07 16:37:06 -0300

    Bugfixes and code cleanup
    - Removed various unnecesary comments
    - Avoid sending a 'play' event for each downloaded piece

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.io.route('stream', function(req) {
         console.log('bad magnet infohash. ignoring');
         return;
     }
+
     var engine = ts('magnet:?xt=urn:btih:' + req.data.hash, options);
     engine.on('ready', function() {
         console.log('engine.onReady');
@@ -28,19 +29,26 @@ app.io.route('stream', function(req) {
         })
     })
 
+    var req = req,
+        signaled = false;
+
     engine.on('download', function() {
+        // Hack due the lack of onFirstPiece (or similar) event
+        if (signaled) {
+            return;
+        }
+
         console.log('engine.onDownload');
         engine.files.forEach(function(file) {
             if (file.name.match(/.*\.(mkv|mp4)/) && file.length > minDownloadSize) {
                 console.log('emit.play: ' + file.path);
-                req.io.emit('play', { src: '/stream/' + file.path})
+                req.io.emit('play', { src: '/stream/' + req.data.hash + '/' + file.path})
+                signaled = true;
             }
         })
     })
 })
 
-// /files/* is accessed via req.params[0]
-// but here we name it :file
 app.get('/stream/:file(*)', function(req, res, next){
   var file = req.params.file
     , path = options.path + '/' + file.replace('..', '');
@@ -48,7 +56,6 @@ app.get('/stream/:file(*)', function(req, res, next){
   res.sendfile(path);
 })
 
-// Send the client html
 app.get('/:file(*)', function(req, res) {
   var file = req.params.file
     , path = __dirname + '/public/' + file.replace('..', '');
