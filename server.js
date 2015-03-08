89707cafb1575274f4f6ecc3edde52f58a28a9fe
DC* <dc@syncr.com.ar>
2015-03-08 16:22:27 -0300

    Fixed onDownload event and play

--- server.js
+++ server.js
@@ -15,35 +15,20 @@ app.io.route('stream', function(req) {
         console.log('bad magnet infohash. ignoring');
         return;
     }
-
-    var engine = ts('magnet:?xt=urn:btih:' + req.data.hash, options);
+    var opts = {
+        path: options.path + req.data.hash
+    };
+    var engine = ts('magnet:?xt=urn:btih:' + req.data.hash, opts);
     engine.on('ready', function() {
         console.log('engine.onReady');
         engine.files.forEach(function(file) {
             if (file.name.match(/.*\.(mkv|mp4)/) && file.length > minDownloadSize) {
-                console.log('download: ' + file.path);
                 var stream = file.createReadStream();
+                var src = '/stream/' + req.data.hash + '/' + file.path;
+                console.log('emit.play: ' + src);
+                req.io.emit('play', { src: src})
             } else {
                 console.log('ignoring: ' + file.path);
-            }
-        })
-    })
-
-    var req = req,
-        signaled = false;
-
-    engine.on('download', function() {
-        // Hack due the lack of onFirstPiece (or similar) event
-        if (signaled) {
-            return;
-        }
-
-        console.log('engine.onDownload');
-        engine.files.forEach(function(file) {
-            if (file.name.match(/.*\.(mkv|mp4)/) && file.length > minDownloadSize) {
-                console.log('emit.play: ' + file.path);
-                req.io.emit('play', { src: '/stream/' + req.data.hash + '/' + file.path})
-                signaled = true;
             }
         })
     })
